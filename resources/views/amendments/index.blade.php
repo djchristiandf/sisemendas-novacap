7dbfe6644303a2c9746307fc97f046fbecf7c350
CHRISTIAN S BARBOSA <32348713+djchristiandf@users.noreply.github.com>
2023-04-24 16:28:18 -0300

    ajuste do datatable para
    Para os botoes de exportacao

--- resources/views/amendments/index.blade.php
+++ resources/views/amendments/index.blade.php
@@ -9,8 +9,50 @@
                 language: {
                     url: '////cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json',
                 },
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf',
+                    {
+                        extend: 'print',
+                        exportOptions: {
+                            columns: [ 0, 1, 2, 3, 4 , 5 , 6 , 7]
+                        },
+                        orientation: 'landscape',
+                        title: 'Emendas NOVACAP',
+                        customize: function(win) {
+                            $(win.document.body)
+                                .css('font-size', '9pt')
+                                .prepend(
+                                    '<img src="http://datatables.net/media/images/logo-fade.png" style="position:absolute; top:0; left:0;" />'
+                                );
+
+                            $(win.document.body).find('table')
+                                .addClass('compact')
+                                .css('font-size', 'inherit');
+
+                            var last = null;
+                            var current = null;
+                            var bod = [];
+
+                            var css = '@page { size: landscape; }',
+                                head = win.document.head || win.document.getElementsByTagName(
+                                    'head')[0],
+                                style = win.document.createElement('style');
+
+                            style.type = 'text/css';
+                            style.media = 'print';
+
+                            if (style.styleSheet) {
+                                style.styleSheet.cssText = css;
+                            } else {
+                                style.appendChild(win.document.createTextNode(css));
+                            }
+
+                            head.appendChild(style);
+                        }
+                    },
+                ]
             });
-            $('#tblAmendment').DataTable();
         });
     </script>
 @endsection
